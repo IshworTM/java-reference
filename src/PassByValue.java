6c0899bda4f0fc4d728bcf92c2aea05186fe11b3
Ishwor Pro OMG wow <dulalishan3@gmail.com>
2024-06-08 22:45:50 +0545

    add passbyvalue

---
+++ src/PassByValue.java
@@ -0,0 +1,14 @@
+package ChapterThree.Lab5;
+
+public class PassByValue {
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println("Before passing by value: " + num);
+        firstFunction(num);
+        System.out.println("After passing by value: " + num);
+    }
+    public static void firstFunction(int num){
+        num = 68;
+        System.out.println("Inside another function: " + num);
+    }
+}
