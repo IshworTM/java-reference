a87d0c455f0d316bf41df966d34c38d5913cb129
Ishwor Pro OMG wow <dulalishan3@gmail.com>
2024-06-08 22:46:47 +0545

    add reference class

---
+++ src/Reference.java
@@ -0,0 +1,16 @@
+package ChapterThree.Lab5;
+
+public class Reference {
+    private String name;
+    public Reference(String name) {
+        this.name = name;
+    }
+
+    public void setReference(String name){
+        this.name = name;
+    }
+
+    public String getReference(){
+        return name;
+    }
+}
